e3b64ea2f9c0f73e04e51f2aaf6a2cb0f4ad579c
Violette Durocher <violette@trendsales.dk>
2024-09-10 15:28:25 +0200

    remove class Card

--- backend/GameStateManager/GameState.py
+++ backend/GameStateManager/GameState.py
@@ -18,17 +18,11 @@ class PlayerMove:
         self.symbol_id = clicked_symbol
 
 
-class Card:
-    def __init__(self, card_id, composition):
-        self.card_id = card_id
-        self.composition = composition
-
-
 class GameStateManager:
     def __init__(self, nb_players, prime_number=7):
         # Create cards deck
         cards = get_cards(prime_number)
-        #np.random.shuffle(cards)
+        np.random.shuffle(cards)
         self.cards = cards
 
         # Select first card as middle card
@@ -67,31 +61,26 @@ class GameStateManager:
                                f"composition = {self.cards[player_cards_ids[0]]}")
         return game_state_str
 
-    def get_top_card_for_player(self, player_id) -> Card:
-        card_id = self.game_state.players_cards[player_id][0]
-        return Card(
-            card_id,
-            self.cards[card_id]
-        )
+    def get_card_composition(self, card_id):
+        return self.cards[card_id]
 
-    def get_middle_card(self) -> Card:
-        card_id = self.game_state.middle_card
-        return Card(
-            card_id,
-            self.cards[card_id]
-        )
+    def get_middle_card_compo(self):
+        return self.get_card_composition(self.game_state.middle_card)
+
+    def get_top_card_compo_for_player(self, player_id):
+        return self.get_card_composition(self.game_state.players_cards[player_id][0])
 
     def valid_player_match(self, move: PlayerMove) -> bool:
         # check the symbol exists both in the player's card and the middle card
-        player_top_card = self.get_top_card_for_player(move.player_id)
-        middle_card = self.get_middle_card()
+        player_top_card_compo = self.get_top_card_compo_for_player(move.player_id)
+        middle_card_compo = self.get_middle_card_compo()
 
-        if move.symbol_id not in player_top_card.composition:
+        if move.symbol_id not in player_top_card_compo:
             # symbol should have not been clickable FIXME
             print("Invalid move")
             return False
 
-        if move.symbol_id not in middle_card.composition:
+        if move.symbol_id not in middle_card_compo:
             # not a match FIXME penalty?
             print("Invalid move")
             return False
