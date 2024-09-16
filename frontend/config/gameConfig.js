5ad7e3c4df6b26094ae7d712f71f682ca7362b39
Jonty Kennedy <jonty@trendsales.dk>
2024-09-16 15:37:51 +0200

    Add frontend for lobbies

---
+++ frontend/config/gameConfig.js
@@ -0,0 +1,15 @@
+export const sizes = {
+  width: 1000,
+  height: 500,
+};
+
+export const gameState = {
+  points: 0,
+  pileSize: 1,
+  playerName: "Andre",
+};
+
+export const gameRules = {
+  totalAmountOfCards: 10,
+  pilePosition: { x: 400, y: 100 },
+};
