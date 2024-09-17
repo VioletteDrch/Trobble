5587278206de896ce7cc14ae5802b627b1f2866f
Violette Durocher <violette@trendsales.dk>
2024-09-17 11:22:11 +0200

    wip

--- backend/game_logic/game_logic_controller.py
+++ backend/game_logic/game_logic_controller.py
@@ -6,11 +6,11 @@ import os
 game_logic_bp = Blueprint('game_logic', __name__)
 
 
-@game_logic_bp.route('/get_symbols')
+@game_logic_bp.route('/get_symbols', methods=['GET'])
 def get_symbols():
     pass
 
 
-@game_logic_bp.route('/get_game_state')
-def get_symbols():
+@game_logic_bp.route('/get_game_state', methods=['GET'])
+def get_game_state():
     pass
