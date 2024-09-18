03d411797f66061e23907d14d0867dcaab869233
Violette Durocher <violette@trendsales.dk>
2024-09-18 14:12:55 +0200

    wip

--- backend/game_logic/game_logic_controller.py
+++ backend/game_logic/game_logic_controller.py
@@ -1,16 +1,25 @@
 from pathlib import Path
+from os import listdir
+
 
 from flask import Blueprint, request, jsonify
+from game_state_elements import GameStateManager
 import os
 
 game_logic_bp = Blueprint('game_logic', __name__)
 
+backend_dir = Path(__file__).resolve().parent.parent
+IMAGES_FOLDER = backend_dir.joinpath('images').joinpath('processed')
+
 
 @game_logic_bp.route('/get_symbols', methods=['GET'])
 def get_symbols():
-    pass
+    symbols_files = listdir(IMAGES_FOLDER)
+    # for s in symbols_files:
+
 
 
 @game_logic_bp.route('/get_game_state', methods=['GET'])
 def get_game_state():
-    pass
+    game = GameStateManager(2, 7)
+    return jsonify(game.game_state)
