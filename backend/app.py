ffd4919507901d7ec05298dda078f273ae0b9623
Amaury <amaury@trendsales.dk>
2024-09-20 12:40:50 +0200

    make the ws server work

--- backend/app.py
+++ backend/app.py
@@ -6,7 +6,7 @@ from threading import Thread
 
 from numpy import False_
 from lobby.lobby_controller import lobby_bp
-from lobby.lobby_websockets import socket_serve
+from lobby.lobby_websockets import serve_bis, socket_serve
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for cross-origin requests
@@ -23,17 +23,16 @@ app.register_blueprint(lobby_bp)
 os.makedirs(RAW_FOLDER, exist_ok=True)
 os.makedirs(PROCESSED_FOLDER, exist_ok=True)
 
-async def start_flask():
+def start_flask():
     print("Starting the flask server")
     app.run(debug=False, host="0.0.0.0")
-    return 1
 
 async def start_servers():
-    tasks = list()
-    if not (app.debug or os.environ.get('FLASK_ENV') == 'development') or os.environ.get('WERKZEUG_RUN_MAIN') == 'true':
-        tasks.append(asyncio.create_task(socket_serve()))
-    tasks.append(asyncio.create_task(start_flask()))
-    await asyncio.gather(*tasks)
+    # tasks = list()
+    # if not (app.debug or os.environ.get('FLASK_ENV') == 'development') or os.environ.get('WERKZEUG_RUN_MAIN') == 'true':
+    #     tasks.append(asyncio.create_task(socket_serve()))
+    # tasks.append(asyncio.create_task(start_flask()))
+    await asyncio.gather(socket_serve(), asyncio.to_thread(start_flask))
 
 if __name__ == '__main__':
     asyncio.run(start_servers())
